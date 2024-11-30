ff87fdb0b2fd63b6cfef791c8c46bf55e6ce8a20
Muhammad Awais Khalid <awais.khalid.awan@gmail.com>
2024-11-30 18:24:47 +0500

    Added Rust tutorial: Parsing a string into integer

--- string_to_int/src/main.rs
+++ string_to_int/src/main.rs
@@ -1,5 +1,6 @@
 use std::io;
 use std::cmp::Ordering;
+use std::process::exit;
 
 fn main() {
     println!("Enter input: ");
@@ -9,7 +10,11 @@ fn main() {
         .read_line(&mut str_number)
         .expect("Unable to get input string.");   // Take an input from user.
 
-    let number: u32 = str_number.trim().parse().expect("Input string is not a number.");   // Parse the input string.
+
+    let number: u32 = match str_number.trim().parse() {  // Parse the input string.
+        Ok(num) => num,                                  // Set the value if the parsing is successful.
+        Err(_) => exit(1),                               // Exit the process with return code 1 if parsing is not successful.
+    };
 
     println!("Input string: {str_number}");         // Print the input string.
     println!("Input string (parsed): {number}");    // Print the parsed integer value.
